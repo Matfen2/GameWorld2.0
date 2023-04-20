a2df46ac426d28c0259b43e28eb516296206b3c6
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2023-04-20 14:33:03 +0200

    first version

--- public/gameWorld.js
+++ public/gameWorld.js
@@ -1,9 +1,38 @@
 var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+});
+
+$(".swiper-slide").each((index, slide) => {
+  $(slide)
+    .find(".card")
+    .each((index, card) => {
+      $(card)
+        .find(".btnPlus")
+        .each((index, btnPlus) => {
+          $(btnPlus).click(() => {
+            $(card).find(".btnPlus").css("display", "none");
+            $(card).find(".btnMinus").css("display", "block");
+            $(card).find(".card-body").toggle();
+          });
+        });
+    });
+});
+
+$(".swiper-slide").each((index, slide) => {
+  $(slide)
+    .find(".card")
+    .each((index, card) => {
+      $(card)
+        .find(".btnMinus")
+        .each((index, btnMinus) => {
+          $(btnMinus).click(() => {
+            $(card).find(".btnPlus").css("display", "block");
+            $(card).find(".btnMinus").css("display", "none");
+            $(card).find(".card-body").toggle();
+          });
+        });
+    });
 });
